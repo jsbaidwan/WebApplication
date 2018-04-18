6de48ce53311ac819ec65ed2636e9285fd4a4e72
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-04-18 11:17:08 +0530

    Comments on Edit page

--- WebApplication1/Pages/Edit.cshtml.cs
+++ WebApplication1/Pages/Edit.cshtml.cs
@@ -14,18 +14,26 @@ namespace WebApplication1.Pages
 
         public EditModel(AppDbContext db) { _db = db; }
 
+        // By default the model binding logic maps the form field names and the property names.
         [BindProperty]
         public Customer Customer { get; set; }
-
+        // In an asynchronous process, the application can continue with other work 
+        // that doesn't depend on the web resource until the potentially blocking task finishes.
+        // OnGetAsyn method loads customer detail to edit page
         public async Task<IActionResult> OnGetAsync(int id)
         {
+            // the method can't continue until the awaited asynchronous operation is complete.
             Customer = await _db.Customers.FindAsync(id);
+            // If no customer record found, return to the index page
             if (Customer == null)
             {
                 return RedirectToPage("/Index");
             }
             return Page();
         }
+        // This method post the changes made in the edit page 
+        // and return back to index page
+        // using Async Task so that page does not get blocked
         public async Task<IActionResult> OnPostAsync()
         {
             // Return to the same page if modelstate is not valid
